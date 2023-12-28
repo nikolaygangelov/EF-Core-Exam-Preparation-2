a55e3354b225f7df10300d772f7876ae3d5ecaa9
Nikolay Angelov <108483720+nikolaygangelov@users.noreply.github.com>
2023-12-28 19:44:43 +0200

    Update Serializer.cs

--- EF Core Exam Preparation 2/Boardgames/DataProcessor/Serializer.cs
+++ EF Core Exam Preparation 2/Boardgames/DataProcessor/Serializer.cs
@@ -11,19 +11,25 @@
     {
         public static string ExportCreatorsWithTheirBoardgames(BoardgamesContext context)
         {
+            //using Data Transfer Object Class to map it with Creators
             XmlSerializer serializer = new XmlSerializer(typeof(ExportCreatorsDTO[]), new XmlRootAttribute("Creators"));
 
+            //using StringBuilder to gather all info in one string
             StringBuilder sb = new StringBuilder();
 
+            //"using" automatically closes opened connections
             using var writer = new StringWriter(sb);
 
             var xns = new XmlSerializerNamespaces();
+
+            //one way to display empty namespace in resulted file
             xns.Add(string.Empty, string.Empty);
 
             var creatorsAndGames = context.Creators
                 .Where(c => c.Boardgames.Any())
                 .Select(c => new ExportCreatorsDTO
                 {
+                    //using identical properties in order to map successfully
                     BoardgamesCount = c.Boardgames.Count,
                     CreatorName = c.FirstName + " " + c.LastName,
                     Boardgames = c.Boardgames
@@ -39,14 +45,20 @@
                 .ThenBy(c => c.CreatorName)
                 .ToArray();
 
+            //Serialize method needs file, TextReader object and namespace to convert/map
             serializer.Serialize(writer, creatorsAndGames, xns);
+
+            //explicitly closing connection in terms of reaching edge cases
             writer.Close();
 
-            return sb.ToString();
+            //using TrimEnd() to get rid of white spaces
+            return sb.ToString().TrimEnd();
         }
 
         public static string ExportSellersWithMostBoardgames(BoardgamesContext context, int year, double rating)
         {
+            //turning needed info about products into a collection using anonymous object
+            //using less data
             var sellersAndBoardgames = context.Sellers
                 .Where(s => s.BoardgamesSellers.Any(bgs => bgs.Boardgame.YearPublished >= year && bgs.Boardgame.Rating <= rating))
                 .Select(s => new
@@ -71,6 +83,8 @@
                 .Take(5)
                 .ToArray();
 
+            //Serialize method needs object to convert/map
+	        //adding Formatting for better reading
             return JsonConvert.SerializeObject(sellersAndBoardgames, Formatting.Indented);
         }
     }
